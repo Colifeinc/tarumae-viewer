b2d00c1ff00f6e5fd6768e83acec82a863481c7a
Jing <jinglu@bulbcorp.jp>
2018-03-30 13:20:06 +0900

    code format

--- src/js/examples/house_shibaura.js
+++ src/js/examples/house_shibaura.js
@@ -1,35 +1,35 @@
 
-// import Tarumae from "../entry"
-// import "../utility/*"
-// import "../scene/*"
+import Tarumae from "../entry";
+import "../utility/*";
+import "../scene/*";
 
-// window.addEventListener('load', function() {
+window.addEventListener("load", function() {
 
-//   const renderer = new Tarumae.Renderer();
+	const renderer = new Tarumae.Renderer();
 
-//   const scene = renderer.createScene();
+	const scene = renderer.createScene();
 
-//   window._scene = scene;
+	window._scene = scene;
  
-//   Tarumae.ResourceManager.download('houses/shibaura/scene.json', 'json', json => {
-//     scene.load(json);
+	Tarumae.ResourceManager.download("houses/shibaura/scene.json", "json", json => {
+		scene.load(json);
 
-//     var panoramic = {
-//       mesh: "models/lightmap/sphere.mesh",
-//       shader: "simple",
-//       location: [0, 3, 0],
-//       scale: [20, 20, 20],
-//       receiveLight: false,
-//       mat: { tex: "textures/sky.jpg", color: [0.7, 0.9, 0.99] },
-//       // mat: { tex: "textures/sky.jpg", color: [1.0,1.1,1.15] },
-//     };
+		var panoramic = {
+			mesh: "models/lightmap/sphere.mesh",
+			shader: "simple",
+			location: [0, 3, 0],
+			scale: [20, 20, 20],
+			receiveLight: false,
+			mat: { tex: "textures/sky.jpg", color: [0.7, 0.9, 0.99] },
+			// mat: { tex: "textures/sky.jpg", color: [1.0,1.1,1.15] },
+		};
     
-//     scene.load(panoramic);
-//   });
+		scene.load(panoramic);
+	});
   
-//   scene.mainCamera.fieldOfView = 90;
+	scene.mainCamera.fieldOfView = 90;
 
-//   new Tarumae.TouchController(scene);
+	new Tarumae.TouchController(scene);
 
-//   scene.show();
-// });
+	scene.show();
+});
