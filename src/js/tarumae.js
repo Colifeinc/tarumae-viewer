55f615b2d09a88824f5094922d1c11fc4cc25a75
Jing <jinglu@bulbcorp.jp>
2018-03-30 13:39:11 +0900

    update references

--- src/js/tarumae.js
+++ src/js/tarumae.js
@@ -7,10 +7,28 @@
 
 import Tarumae from "./entry";
 
-import "./utility/*";
-import "./math/*";
-import "./scene/*";
-import "./webgl/*";
+import "./utility/archive.js";
+import "./utility/debug.js";
+import "./utility/event.js";
+import "./utility/res.js";
+import "./utility/stopwatch.js";
+import "./utility/utility.js";
+import "./math/functions.js";
+import "./math/matrix.js";
+import "./math/spacetree.js";
+import "./math/vector.js";
+import "./scene/animation.js";
+import "./scene/camera.js";
+import "./scene/material.js";
+import "./scene/object.js";
+import "./scene/renderer.js";
+import "./scene/scene.js";
+import "./scene/viewer.js";
+import "./webgl/buffers.js";
+import "./webgl/cubemap.js";
+import "./webgl/mesh.js";
+import "./webgl/shader.js";
+import "./webgl/texture.js";
 
 export default Tarumae;
 export { Vec2, Vec3, Vec4, Color3, Color4 } from "./math/vector";
