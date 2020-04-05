1704ee9606a71c926c2a625a3bde8b825a823cfa
Jingwood <dujid0@gmail.com>
2020-04-06 02:13:31 +0900

    fix

--- src/js/utility/objloader.js
+++ src/js/utility/objloader.js
@@ -14,21 +14,21 @@ const faceTrianglePattern = `^f\s+${ triangleIndexPattern }\s+`;
 function readLine(s, line) {
   let matches;
 
-  if ((matches = line.match(/^v\s+([\-\d\.]+)\s+([\-\d\.]+)\s+([\-\d\.]+)/ig))) {
-    addIntoArr(s.vertices, matches[1], matches[2], matches[3]);
+  if ((matches = line.match(/^v\s+([\-\d\.]+)\s+([\-\d\.]+)\s+([\-\d\.]+)/))) {
+    addVector(s.vertices, matches[1], matches[2], matches[3]);
   }
-  else if ((matches = line.match(/^vn\s+([\-\d\.]+)\s+([\-\d\.]+)\s+([\-\d\.]+)/ig))) {
-    addIntoArr(s.normals, matches[1], matches[2], matches[3]);
+  else if ((matches = line.match(/^vn\s+([\-\d\.]+)\s+([\-\d\.]+)\s+([\-\d\.]+)/))) {
+    addVector(s.normals, matches[1], matches[2], matches[3]);
   }
-  else if ((matches = line.match(/^f\s+(\d+)\/(\d+)\/(\d+)\s+(\d+)\/(\d+)\/(\d+)\s+(\d+)\/(\d+)\/(\d+)/ig))) {
+  else if ((matches = line.match(/^f\s+(\d+)\/(\d+)\/(\d+)\s+(\d+)\/(\d+)\/(\d+)\s+(\d+)\/(\d+)\/(\d+)/))) {
     s.faces.push([matches[1], matches[2], matches[3],
       matches[4], matches[5], matches[6],
       matches[7], matches[8], matches[9]].map(e => new Number(e)));
   }
 }
 
-function addIntoArr(arr, x, y , z) {
-  arr.push(new Number(x), new Number(y), new Number(z));
+function addVector(arr, x, y, z) {
+  arr.push({ x: new Number(x), y: new Number(y), z: new Number(z) });
 }
 
 Tarumae.loadObjFormat = function(text) {
@@ -36,7 +36,8 @@ Tarumae.loadObjFormat = function(text) {
     vertices: [],
     normals: [],
     texcoords: [],
-    faces:[],
+    faces: [],
+    vertexBuffer: [],
   };
 
   const arrayOfLines = text.match(/[^\r\n]+/g);
@@ -45,13 +46,25 @@ Tarumae.loadObjFormat = function(text) {
   }
 
   console.log(s.vertices.length);
-  console.log(s.normals.length);
-  console.log(s.faces);
+
+  for (const face of s.faces) {
+    console.log(face[0], face[3], face[6]);
+    const v1 = s.vertices[face[0]-1], v2 = s.vertices[face[3]-1], v3 = s.vertices[face[6]-1];
+    s.vertexBuffer.push(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z);
+  }
+
+  console.log(s.faces.length);
+  console.log(s.vertexBuffer.length);
 
   const obj = new Tarumae.SceneObject();
   obj.mat = {};
 
   const mesh = new Tarumae.Mesh();
+  mesh.vertexBuffer = s.vertexBuffer;
+  mesh.meta = {
+    vertexCount: s.faces.length,
+  };
+
   obj.meshes.push(mesh);
   
   return obj;
