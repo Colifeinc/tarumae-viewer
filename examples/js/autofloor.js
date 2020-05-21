76c347a95974ceb4653da3e4b4a202b17f3b2185
Jingwood <dujid0@gmail.com>
2020-05-21 12:34:20 +0900

    add storyboard and keyframe changer

--- examples/js/autofloor.js
+++ examples/js/autofloor.js
@@ -10,128 +10,130 @@ import { Vec3, Color4 } from "@jingwood/graphics-math";
 
 window.addEventListener("load", function() {
 
-	const renderer = new Tarumae.Renderer({
-		enableLighting: false,
-		backColor: new Color4(0.96, .98, 1, 1),
+  const renderer = new Tarumae.Renderer({
+    enableLighting: false,
+    backColor: new Color4(0.96, .98, 1, 1),
 		
-		enablePostprocess: true,
-		renderingImage: {
-			gamma: 1.2,
-			resolutionRatio: 1,
-		},
-		enableAntialias: true,
-		enableShadow: true,
-		shadowQuality: {
-			scale: 10,
-			viewDepth: 3,
-			resolution: 512,
-			intensity: 0.23,
+    enablePostprocess: true,
+    renderingImage: {
+      gamma: 1.2,
+      resolutionRatio: 1,
+    },
+    enableAntialias: true,
+    enableShadow: true,
+    shadowQuality: {
+      scale: 10,
+      viewDepth: 3,
+      resolution: 512,
+      intensity: 0.23,
       enableCache: false,
-		},
-		bloomEffect: {
-			threshold: 0.3,
-			gamma: 1.4,
-		},
-	});
+    },
+    bloomEffect: {
+      threshold: 0.3,
+      gamma: 1.4,
+    },
+  });
 
-	const scene = renderer.createScene();
-	window._scene = scene;
+  const scene = renderer.createScene();
+  window._scene = scene;
 	
-	let rootObj, refCubebox;
+  let rootObj, refCubebox;
 
-	const setObjectRefmap = (obj) => {
-		obj.eachChild(c => {
-			if (c.meshes.length > 0) c.meshes[0]._refmap = refCubebox.cubemap;
-			if (c.mat && !c.mat.glossy) {
-				c.mat.glossy = 0.1;
-			}
-		});
-	};
+  const setObjectRefmap = (obj) => {
+    obj.eachChild(c => {
+      if (c.meshes.length > 0) c.meshes[0]._refmap = refCubebox.cubemap;
+      if (c.mat && !c.mat.glossy) {
+        c.mat.glossy = 0.1;
+      }
+    });
+  };
 
-	const showcaseToba = "floors/floor-ecomode.toba";
-	scene.createObjectFromURL(showcaseToba, obj => {
-		const floorObj = obj.findObjectByName("floor");
+  const showcaseToba = "floors/floor-afgen-output.toba";
+  scene.createObjectFromURL(showcaseToba, obj => {
+    const floorObj = obj.findObjectByName("floor");
 	
-		if (floorObj) {
-			floorViewController.targetObject = floorObj;
-			scene.mainCamera.collisionMode = Tarumae.CollisionModes.NavMesh;
-			scene.mainCamera.collisionTarget = floorObj;
-		}
+    if (floorObj) {
+      floorViewController.targetObject = floorObj;
+      scene.mainCamera.collisionMode = Tarumae.CollisionModes.NavMesh;
+      scene.mainCamera.collisionTarget = floorObj;
+    }
 
-		const wall = obj.findObjectByName("wall");
-		if (wall) wall.mat.color = [1, 1, 1];
+    const wall = obj.findObjectByName("wall");
+    if (wall) wall.mat.color = [1, 1, 1];
 
-		obj.eachChild(child => {
-			child.receiveShadow = child === floorObj;
-		});
+    obj.eachChild(child => {
+      child.receiveShadow = child === floorObj;
+    });
 		
-		scene.add(obj);
+    scene.add(obj);
 
-		obj.scale.set(0.00001, 0.00001, 0.00001);
+    obj.scale.set(0.00001, 0.00001, 0.00001);
 
-		rootObj = obj;
+    rootObj = obj;
 
-		if (refCubebox) {
-			setObjectRefmap(obj);
-		}
+    if (refCubebox) {
+      setObjectRefmap(obj);
+    }
 
-		setTimeout(() => {
-			scene.animate({duration: 0.7}, t => {
-				const s = Math.sin(t * 2);
-				obj.scale.set(s, s, s);
+    setTimeout(() => {
+      scene.animate({ duration: 0.7 }, t => {
+        const s = Math.sin(t * 2);
+        obj.scale.set(s, s, s);
       }, _ => {
-          renderer.options.shadowQuality.enableCache = true;
+        renderer.options.shadowQuality.enableCache = true;
       });
-		}, 500);
-	});
+    }, 500);
+  });
 
-	let baseurl = "textures/cubemap/city/"
+  let baseurl = "textures/cubemap/city/"
   const skyImageUrls = [
     baseurl + "px.jpg", baseurl + "nx.jpg", baseurl + "py.jpg",
     baseurl + "ny.jpg", baseurl + "pz.jpg", baseurl + "nz.jpg",
-	];
+  ];
 
-	scene.skybox = new Tarumae.SkyBox(renderer, skyImageUrls);
-	scene.skybox.visible = false;
+  scene.skybox = new Tarumae.SkyBox(renderer, skyImageUrls);
+  scene.skybox.visible = false;
 
-	// baseurl = "textures/cubemap/office-256-blur/"
+  // baseurl = "textures/cubemap/office-256-blur/"
   // const refBoxUrls = [
   //   baseurl + "px.jpg", baseurl + "nx.jpg", baseurl + "py.jpg",
   //   baseurl + "ny.jpg", baseurl + "pz.jpg", baseurl + "nz.jpg",
-	// ];
+  // ];
 	
-	// refCubebox = new Tarumae.ImageCubeBox(renderer, refBoxUrls);
+  // refCubebox = new Tarumae.ImageCubeBox(renderer, refBoxUrls);
 
-	// refCubebox.on('load', _ => {
-	// 	if (rootObj) {
-	// 		setObjectRefmap(rootObj);
-	// 	}
+  // refCubebox.on('load', _ => {
+  // 	if (rootObj) {
+  // 		setObjectRefmap(rootObj);
+  // 	}
   // });
 
-	const floorViewController = new Tarumae.FloorViewController(scene);
+  const floorViewController = new Tarumae.FloorViewController(scene);
   
   floorViewController.on("beginChangeMode", _ => {
     if (floorViewController.topViewStatus.topViewMode) {
       if (scene.skybox) {
-				scene.skybox.visible = false;
-			}
-		}
+        scene.skybox.visible = false;
+      }
+    }
   });
   
-	floorViewController.on("modeChanged", _ => {
+  floorViewController.on("modeChanged", _ => {
     if (!floorViewController.topViewStatus.topViewMode) {
       if (scene.skybox) {
         scene.skybox.visible = true;
       }
+
+      sb.play();
     }
 
     // cache the shadowmap on first time the mode changed
     renderer.options.shadowQuality.enableCache = true;
-	});
+  });
 
-	scene.sun.location.set(0, 10, 0);
-	scene.sun.mat.color = [1.1, 1.1, 1.1];
+  scene.sun.location.set(0, 10, 0);
+  scene.sun.mat.color = [1.1, 1.1, 1.1];
 
-	scene.show();
+  scene.show();
 });
 
--- src/js/scene/animation.js
+++ src/js/scene/animation.js
@@ -6,229 +6,268 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 import Tarumae from "../entry";
-import { MathFunctions } from "@jingwood/graphics-math";
+import { Vec3, MathFunctions } from "@jingwood/graphics-math";
 
 Tarumae.Animation = class {
 	
-	constructor(scene, options, onframe, onfinish) {
-		this.scene = scene;
+  constructor(scene, options, onframe, onfinish) {
+    this.scene = scene;
 
-		this.startTime = undefined;
-		this.endTime = undefined;
+    this.startTime = undefined;
+    this.endTime = undefined;
 
-		const _options = { ...Tarumae.Animation.DefaultOptions, ...options };
-		this.duration = _options.duration || 1;
-		this.delay = _options.delay || 0;
-		this.repeat = _options.repeat || 0;
+    const _options = { ...Tarumae.Animation.DefaultOptions, ...options };
+    this.duration = _options.duration || 1;
+    this.delay = _options.delay || 0;
+    this.repeat = _options.repeat || 0;
 
-		this.isPaused = false;
-		this.elapsedTime = 0;
-		this.repeatCount = 0;
-		this.initialized = false;
+    this.isPausedOrStop = false;
+    this.elapsedTime = 0;
+    this.repeatCount = 0;
+    this.initialized = false;
   
-		if (typeof options.name === "string" && options.name.length > 0) {
-			this.name = options.name;
-		} else {
-			this.name = Tarumae.Animation.getAvailableDefaultName();
-		}
+    if (typeof options.name === "string" && options.name.length > 0) {
+      this.name = options.name;
+    } else {
+      this.name = Tarumae.Animation.getAvailableDefaultName();
+    }
   
-		if (typeof onframe === "function") {
-			this.onframe = onframe;
-		}
+    if (typeof onframe === "function") {
+      this.onframe = onframe;
+    }
 
-		if (typeof onfinish === "function") {
-			this.on("finish", onfinish);
-		}
-	}
+    if (typeof onfinish === "function") {
+      this.on("finish", onfinish);
+    }
+  }
 
-	get progressRate() {
-		if (this._inDelay) return 0;
-		return this.elapsedTime / this._msDuration;
-	}
+  get progressRate() {
+    if (this._inDelay) return 0;
+    return this.elapsedTime / this._msDuration;
+  }
 
-	get isPlaying() {
-		return this.elapsedTime < this.duration * 1000;
-	}
+  get isPlaying() {
+    return !this.isPausedOrStop && this.elapsedTime < this.duration * 1000;
+  }
 
-	get isDuringDelay() {
-		return this._inDelay;
-	}
+  get isDuringDelay() {
+    return this._inDelay;
+  }
 
-	get isFinished () {
-		return this.elapsedTime >= this._msDuration;
-	}
+  get isFinished() {
+    return this.elapsedTime >= this._msDuration;
+  }
 
-	initialize() {
-		this._msDuration = this.duration * 1000;
-		this._lastCheckedTime = undefined;
+  initialize() {
+    this._msDuration = this.duration * 1000;
+    this._lastCheckedTime = undefined;
 
-		this._inDelay = this.delay > 0;
+    this._inDelay = this.delay > 0;
 
-		this.startTime = Date.now();
-		this.endTime = this.startTime + this.duration * 1000;
+    this.startTime = Date.now();
+    this.endTime = this.startTime + this.duration * 1000;
     
-		this.initialized = true;
-	}
+    this.initialized = true;
+  }
 
-	play() {
-		if (this.delay) {
-			setTimeout(_ => {
-				this.play();
-			}, this.delay * 1000);
-			this.delay = 0;
-			return;
-		}
+  play() {
+    if (this.delay) {
+      setTimeout(_ => this.play(), this.delay * 1000);
+      this.delay = 0;
+      return;
+    }
 
-		if (typeof this.onframe !== "function") {
-			throw Error("must specify the onframe function to start animation.");
-		}
+    if (typeof this.onframe !== "function") {
+      throw Error("must specify the onframe function to start animation.");
+    }
 
-		if (!this.initialized) {
-			this.initialize();
-		}
+    if (!this.initialized) {
+      this.initialize();
+    }
     
-		if (typeof this.name === "string" && this.name.length > 0) {
-			Tarumae.Animation.cancelAnimationByName(this.name);
-			Tarumae.Animation.RunningAnimations[this.name] = this;
-		}
+    if (typeof this.name === "string" && this.name.length > 0) {
+      Tarumae.Animation.cancelAnimationByName(this.name);
+      Tarumae.Animation.RunningAnimations[this.name] = this;
+    }
 
-		// resume animation when play is called
-		this.isPaused = false;
-		this._lastCheckedTime = Date.now();
+    // resume animation when play is called
+    this.isPausedOrStop = false;
+    this._lastCheckedTime = Date.now();
 
-		// this.createAnimationTimer();
-		requestAnimationFrame(_ => this.tick());
-	}
+    // this.createAnimationTimer();
+    requestAnimationFrame(_ => this.tick());
+  }
 
-	tick() {
-		const now = Date.now();
-		const diff = now - this._lastCheckedTime;
-		this._lastCheckedTime = now;
+  tick() {
+    const now = Date.now();
+    const diff = now - this._lastCheckedTime;
+    this._lastCheckedTime = now;
 
-		if (this.isPaused) return;
+    if (this.isPausedOrStop) return;
     
-		this.elapsedTime += diff;
+    this.elapsedTime += diff;
 
-		if (this._inDelay) {
-			if (this.elapsedTime < this.delay * 1000) {
-				return;
-			} else {
-				this._inDelay = false;
-				this.elapsedTime = 0;
-			}
-		}
+    if (this._inDelay) {
+      if (this.elapsedTime < this.delay * 1000) {
+        return;
+      } else {
+        this._inDelay = false;
+        this.elapsedTime = 0;
+      }
+    }
     
-		if (this.elapsedTime < this._msDuration) {
-			let t = this.elapsedTime / this._msDuration;
+    if (this.elapsedTime < this._msDuration) {
+      let t = this.elapsedTime / this._msDuration;
     
-			switch (this.effect) {
-			default:
-			case "smooth":
-				t = MathFunctions.smoothstep(0, 1, t);
-				break;
+      switch (this.effect) {
+        default:
+        case "smooth":
+          t = MathFunctions.smoothstep(0, 1, t);
+          break;
 
-			case "sharp":
-				t = MathFunctions.smoothstep(0.2, 0.8, t);
-				break;
+        case "sharp":
+          t = MathFunctions.smoothstep(0.2, 0.8, t);
+          break;
 
-			case "fadein":
-				t = 1 - Math.cos(Math.PI / 2 * t);
-				break;
+        case "fadein":
+          t = 1 - Math.cos(Math.PI / 2 * t);
+          break;
 
-			case "fadeout":
-				t = Math.sin(Math.PI / 2 * t);
-				break;
-			}
+        case "fadeout":
+          t = Math.sin(Math.PI / 2 * t);
+          break;
+      }
 
-			this.onframe(t);
-			if (this.scene) this.scene.requireUpdateFrame();
+      this.onframe(t);
+      if (this.scene) this.scene.requireUpdateFrame();
 			
-			requestAnimationFrame(_ => this.tick());
-			return;
-		}
+      requestAnimationFrame(_ => this.tick());
+      return;
+    }
 
-		if (!isFinite(this.repeat)) {
-			this.elapsedTime = 0;
-			return;
-		} else if (this.repeat > 1) {
-			this.repeatCount++;
+    if (!isFinite(this.repeat)) {
+      this.elapsedTime = 0;
+      return;
+    } else if (this.repeat > 1) {
+      this.repeatCount++;
 
-			if (this.repeatCount < this.repeat) {
-				this.elapsedTime = 0;
-				return;
-			}
-		}
+      if (this.repeatCount < this.repeat) {
+        this.elapsedTime = 0;
+        return;
+      }
+    }
 
-		this.onframe(1);
-		if (this.scene) this.scene.requireUpdateFrame();
+    this.onframe(1);
+    if (this.scene) this.scene.requireUpdateFrame();
 
-		this.removeAnimationTimer();
-		this.onfinish();
-	}
+    this.removeAnimationTimer();
+    this.onfinish();
+  }
 
-	// createAnimationTimer() {
-	// 	if (!this.timer) {
-	// 		var _this = this;
-	// 		this.timer = setInterval(function() { _this.tick() }, Tarumae.Animation.Interval);
-	// 	}
-	// }
+  // createAnimationTimer() {
+  // 	if (!this.timer) {
+  // 		var _this = this;
+  // 		this.timer = setInterval(function() { _this.tick() }, Tarumae.Animation.Interval);
+  // 	}
+  // }
 
-	removeAnimationTimer() {
-		// if (this.timer) {
-		// 	clearInterval(this.timer);
-		// 	this.timer = undefined;
-		// }
+  removeAnimationTimer() {
+    // if (this.timer) {
+    // 	clearInterval(this.timer);
+    // 	this.timer = undefined;
+    // }
 
-		if (typeof this.name === "string" && this.name.length > 0) {
-			var previousInstance = Tarumae.Animation.RunningAnimations[this.name];
-			if (previousInstance) {
-				delete Tarumae.Animation.RunningAnimations[this.name];
-			}
-		}
-	}
+    if (typeof this.name === "string" && this.name.length > 0) {
+      const previousInstance = Tarumae.Animation.RunningAnimations[this.name];
+      if (previousInstance) {
+        previousInstance.isPausedOrStop = true;
+        delete Tarumae.Animation.RunningAnimations[this.name];
+      }
+    }
+  }
 
-	stop() {
-		this.removeAnimationTimer();
-		this.onstop();
-	}
+  stop() {
+    this.removeAnimationTimer();
+    this.onstop();
+  }
 
-	pause() {
-		// if (this.timer) {
-		// 	clearInterval(this.timer);
-		// 	this.timer = undefined;
-		// }
-		this.isPaused = true;
+  pause() {
+    // if (this.timer) {
+    // 	clearInterval(this.timer);
+    // 	this.timer = undefined;
+    // }
+    this.isPausedOrStop = true;
 
-		this.onpause();
-	}
+    this.onpause();
+  }
 
-	reset() {
-		this.elapsedTime = 0;
-	}
+  reset() {
+    this.elapsedTime = 0;
+  }
 	
-	static isAnyAnimationPlaying() {
-		return !Tarumae.Animation.RunningAnimations._s3_isEmpty();
-	}
+  static isAnyAnimationPlaying() {
+    return !Tarumae.Animation.RunningAnimations._s3_isEmpty();
+  }
 
-	static isAnimationPlaying(name) {
-		return Tarumae.Animation.RunningAnimations.hasOwnProperty(name);
-	}
+  static isAnimationPlaying(name) {
+    return Tarumae.Animation.RunningAnimations.hasOwnProperty(name);
+  }
 
-	static cancelAnimationByName(name) {
-		var previousInstance = Tarumae.Animation.RunningAnimations[name];
-		if (previousInstance) {
-			// if (previousInstance.timer) clearInterval(previousInstance.timer);
-			delete Tarumae.Animation.RunningAnimations[name];
-		}
-	}
+  static cancelAnimationByName(name) {
+    var previousInstance = Tarumae.Animation.RunningAnimations[name];
+    if (previousInstance) {
+      // if (previousInstance.timer) clearInterval(previousInstance.timer);
+      delete Tarumae.Animation.RunningAnimations[name];
+    }
+  }
 
-	static getAvailableDefaultName() {
-		var name;
-		while (name === undefined || Tarumae.Animation.RunningAnimations.hasOwnProperty(name)) {
-			name = "__unnamed" + Date.now() + Math.floor(Math.random());
-		}
-		return name;
-	}
+  static getAvailableDefaultName() {
+    var name;
+    while (name === undefined || Tarumae.Animation.RunningAnimations.hasOwnProperty(name)) {
+      name = "__unnamed" + Date.now() + Math.floor(Math.random());
+    }
+    return name;
+  }
+  
+}
+
+Tarumae.Animation.PropertyChanger = class {
+  constructor(define, options) {
+    this.define = define;
+    this.options = options;
+  }
+  
+  start() {
+    const define = this.define;
+
+    this.object = typeof define.object === "function" ? define.object() : define.object;
+    this.property = typeof define.property === "function" ? define.property() : define.property;
+    
+    this.value = this.object[this.property];
+    this.valueType = typeof this.value;
+    
+    if (typeof define.start === "function") {
+      this.startValue = define.start(this.value);
+    } else if (typeof define.start !== "undefined") {
+      this.startValue = define.start;
+    } else {
+      if (this.value instanceof Vec3) {
+        this.startValue = new Vec3(this.value);
+      } else {
+        this.startValue = this.value;
+      }
+    }
+
+    this.endValue = typeof define.end === "function" ? define.end(this.value) : define.end;
+  }
+  
+  frame(t) {
+    if (this.value instanceof Vec3) {
+      this.object[this.property] = Vec3.lerp(this.startValue, this.endValue, t);
+    } else if (this.valueType === "number") {
+      this.object[this.property] = (this.startValue + (this.endValue - this.startValue) * t);
+    }
+  }
 }
 
 Object.assign(Tarumae.Animation, {
@@ -256,51 +295,64 @@ new Tarumae.EventDispatcher(Tarumae.Animation).registerEvents(
 	"finish", "pause", "play", "stop"
 );
 
-Tarumae.Storyboard = function(scene, timeline) {
-	this.scene = scene;
-	this.segments = [];
-	this.currentSegment = 0;
-	this.playing = false;
+Tarumae.Storyboard = class {
+  constructor(scene, timeline) {
+    this.scene = scene;
+    this.keyframes = [];
+    this.keyStep = 0;
+    this.playing = false;
+    this.currentAnimation = null;
+  
+    if (Array.isArray(timeline)) {
+      for (const keyframe of timeline) {
+        this.add(keyframe);
+      }
+    }
+  }
 
-	if (Array.isArray(timeline)) {
-		for (var i = 0; i < timeline.length; i++) {
-			var seg = timeline[i];
-			this.add(seg[0], seg[1]);
-		}
-	}
+  add(keyframe) {
+    this.keyframes.push(keyframe);
+  }
+
+  play() {
+    if (!this.playing) {
+      this.playing = true;
+      this.playNextKeyFrame();
+    }
+  }
+
+  stop() {
+    if (this.currentAnimation) {
+      this.currentAnimation.stop();
+    }
+    this.keyStep = 0;
+    this.playing = false;
+  }
+
+  playNextKeyFrame() {
+    const keyframe = this.keyframes[this.keyStep];
+    keyframe.start();
+    this.onkeyframeBegin(keyframe, this.keyStep);
+
+    this.currentAnimation = this.scene.animate(keyframe.options, t => {
+      keyframe.frame(t, this.keyStep);
+    }, _ => {
+      this.onkeyframeEnd(keyframe, this.keyStep);
+
+      this.keyStep++;
+
+      if (this.keyStep < this.keyframes.length) {
+        this.playNextKeyFrame();
+      } else {
+        this.currentAnimation = null;
+        this.stop();
+
+        this.onfinish(this.keyStep);
+      }
+    });
+  }
 };
 
-Tarumae.Storyboard.prototype = {
-	add: function(settings, action) {
-		this.segments.push({ settings: settings, action: action });
-	},
-
-	play: function() {
-		if (!this.playing) {
-			this.playing = true;
-			this.playNextSegment();
-		}
-	},
-
-	playNextSegment: function() {
-		var seg = this.segments[this.currentSegment];
-		Tarumae.Utility.invokeIfExist(seg, "onbegin");
-
-		var sb = this;
-
-		this.scene.animate(seg.settings, seg.action, function() {
-			Tarumae.Utility.invokeIfExist(seg, "onend");
-
-			sb.currentSegment++;
-
-			if (sb.currentSegment < sb.segments.length) {
-				sb.playNextSegment();
-			} else {
-				sb.currentSegment = 0;
-				sb.playing = false;
-
-				Tarumae.Utility.invokeIfExist(sb, "onfinish");
-			}
-		});
-	},
-};
+new Tarumae.EventDispatcher(Tarumae.Storyboard).registerEvents(
+	"keyframeBegin", "keyframeEnd", "finish",
+);
